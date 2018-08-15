a54f69594a374a47b5b5e643ad12e50281225f4d
liuzhongkai <sdhzlzhk@126.com>
2018-08-16 00:50:31 +0800

    修改trident客户端访问的bug

--- src/main/java/com/glodon/safe/stream/trident/client/WordCountClient.java
+++ src/main/java/com/glodon/safe/stream/trident/client/WordCountClient.java
@@ -1,6 +1,5 @@
 package com.glodon.safe.stream.trident.client;
 
-import com.glodon.safe.stream.spout.RandomSentenceSpout;
 import org.apache.storm.Config;
 import org.apache.storm.generated.AuthorizationException;
 import org.apache.storm.generated.DRPCExecutionException;
@@ -14,9 +13,13 @@ public class WordCountClient {
     private static final Logger LOG = LoggerFactory.getLogger(WordCountClient.class);
     public static void main(String[] args) {
         Config conf = new Config();
+        conf.put("storm.thrift.transport", "org.apache.storm.security.auth.plain.PlainSaslTransportPlugin");
+        conf.put(Config.STORM_NIMBUS_RETRY_TIMES, 3);
+        conf.put(Config.STORM_NIMBUS_RETRY_INTERVAL, 10);
+        conf.put(Config.STORM_NIMBUS_RETRY_INTERVAL_CEILING, 20);
         try {
             DRPCClient client = new DRPCClient(conf,"cluster01", 3772);
-            String result = client.execute("words", "黄河之水天上来，奔流到海不复回");
+            String result = client.execute("words", "泪");
             LOG.info("结果是：{}", result);
         } catch (TTransportException e) {
             e.printStackTrace();
